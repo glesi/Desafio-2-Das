a888978ad517f631ada9b61a2dd2093a1781c59a
Jonathan Reyes <134872289+JONHREYES95@users.noreply.github.com>
2024-05-24 20:32:15 -0600

    mvc
    mvc

---
+++ Cine+/Cine+/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cine_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
